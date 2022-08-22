25d83e2a4af004d78b540a47b3146a94716db6e4
Anton Golubenko <anton.golubenko@gmail.com>
2022-08-22 15:40:34 +0200

    implementing a search bar

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
 import { colors } from './src/utils/colors';
+import { Searchbar } from 'react-native-paper';
 
 const isAndroid = Platform.OS === 'android';
 
@@ -9,8 +10,12 @@ export default function App() {
   return (
     <>
     <SafeAreaView style={styles.container}>
-      <View style={styles.greenCont}>
-        <Text style={styles.text}> Hey </Text>
+      <View style={styles.searchBar}>
+          <Searchbar
+            placeholder="Search for restaurants"
+            //onChangeText={this.updateSearch}
+           // value={search}
+          />
       </View>
       <View style={styles.blueCont}>
         <Text style={styles.text}> Bye </Text>
@@ -27,9 +32,8 @@ const styles = StyleSheet.create({
       flex: 1,
       marginTop: StatusBar.currentHeight
     },
-    greenCont: {
-      padding: 2,
-      backgroundColor: colors.green
+    searchBar: {
+      padding: 20
     },
     blueCont: {
       flex: 1,
